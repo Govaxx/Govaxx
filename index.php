341023dfc6644066cd0a1ab5819b127cd586a66e
Jrmy77 <60353764+Jrmy77@users.noreply.github.com>
2022-02-18 08:56:45 +0100

    Govaxx

---
+++ index.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+	
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="style.css"/>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<title>Govax</title>
+	</head>
+
+	<body>
+        <nav class="navbar navbar-expand-sm navbar-dark ">
+           <a class="navbar-brand" href="#">Govax</a>
+  
+           <!-- Left -->
+           <ul class="navbar-nav mr-auto">
+              <li class="nav-item">
+                 <a class="nav-link">menu1</a>
+              </li>
+              <li class="nav-item">
+                 <a class="nav-link">menu2</a>
+              </li>
+           </ul>
+           <!-- Right -->
+           <ul class="navbar-nav ml-auto">
+             <li class="nav-item">
+                <a class="nav-link" href="account.php">Mon compte</a>
+             </li>
+           </ul>
+        </nav>
+  
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+     </body>
+</html>
